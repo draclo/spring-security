d778757f57fd487e2197615f263136a8e3db6a7a
draclo <draclo@naver.com>
2023-02-07 21:07:24 +0900

    2) 정적 자원 관리 - Webignore 설정

---
+++ src/main/java/io/security/springsecurity/domain/Account.java
@@ -0,0 +1,21 @@
+package io.security.springsecurity.domain;
+
+import lombok.Data;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+@Data
+public class Account {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+    private String username;
+    private String password;
+    private String email;
+    private Integer age;
+    private String role;
+}
