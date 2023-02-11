ec30b7e5ff514f2e54a4e83aefe18bf9633a0c4a
draclo <draclo@naver.com>
2023-02-11 23:13:59 +0900

    6) 커스텀 로그인 페이지 생성하기

---
+++ src/main/java/io/security/springsecurity/controller/login/LoginController.java
@@ -0,0 +1,13 @@
+package io.security.springsecurity.controller.login;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class LoginController {
+
+    @GetMapping("/login")
+    public String loginPage() {
+        return "user/login/login";
+    }
+}
